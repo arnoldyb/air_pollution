7fa251b31bf94fa6f38a6434bb72ad2f0f573ebe
Angshuman Paul <paulang@berkeley.edu>
2019-09-18 20:16:13 -0700

    python file for reading and storing purple air data

---
+++ initial_looks/readData.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+import pandas as pd
+import datetime, time
+from fastparquet import write
+
+import warnings
+warnings.filterwarnings('ignore')
+
+while True:
+    now = datetime.datetime.now()
+    datetimeval = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    parquet_file = "data.parquet"
+    if now.minute % 5 == 4 and now.second == 56:
+        data_df = pd.read_json(path_or_buf="https://www.purpleair.com/json")
+        write(parquet_file, data_df,compression='GZIP')
+        os.system("aws s3 cp data.parquet s3://midscapstone-whos-polluting-my-air/PurpleAir/{}.parquet".format(datetimeval))
+        time.sleep(120)
