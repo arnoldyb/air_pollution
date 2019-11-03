31e99cf2e7af9aa0f8a167185af79897578003ff
Ubuntu <markpaluta@gmail.com>
2019-11-03 01:04:51 +0000

    refactoring build_model.py to make more sense

--- VirtualSensing/build_model.py
+++ VirtualSensing/build_model.py
@@ -7,6 +7,7 @@ from copy import deepcopy
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+from joblib import dump, load
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("../HistoricalData/")
@@ -38,36 +39,44 @@ def import_data():
     return data_df
 
 
-def build_kNN_model():
-    
+def split_data(columns_to_keep, stratify=True):
+
+    if stratify:
+        # stratifying by sensor_id to avoid data_leakage
+        id_train_dev, id_test = train_test_split(data_df.sensor_id.unique(), test_size=.2)
+        id_train, id_dev = train_test_split(id_train_dev, test_size=.125)
+
+        data_df_train = data_df[data_df.sensor_id.isin(id_train)]
+        data_df_dev = data_df[data_df.sensor_id.isin(id_dev)]
+        data_df_test = data_df[data_df.sensor_id.isin(id_test)]
+        
+        X_train = data_df_train[columns_to_keep]
+        X_dev = data_df_dev[columns_to_keep]
+        X_test = data_df_test[columns_to_keep]
+        y_train = data_df_train['2_5um']
+        y_dev = data_df_dev['2_5um']
+        y_test = data_df_test['2_5um']
+    else:
+        # standard train-dev-test split
+        X_data_df = data_df[columns_to_keep]
+        y_data_df = data_df['2_5um']
+
+        X_train_and_dev, X_test, y_train_and_dev, y_test = train_test_split(X_data_df, y_data_df, test_size=0.20, random_state=42)
+        X_train, X_dev, y_train, y_dev = train_test_split(X_train_and_dev, y_train_and_dev, test_size=0.125, random_state=42)
+        
+    return (X_train, X_dev, X_test, y_train, y_dev, y_test)
+
+
+def build_kNN_model(data_df):
+    # turn datetime into an integer so that a model can fit on it
     data_df['time_delta'] = (data_df['created'] - pd.Timestamp('2019-09-01 00:00:00')) / np.timedelta64(1, 'm')
-
-    columns_to_keep = ['time_delta', 'lat', 'lon'] 
-
-    # stratifying by sensor_id to avoid data_leakage
-    id_train_dev, id_test = train_test_split(data_df.sensor_id.unique(), test_size=.2)
-    id_train, id_dev = train_test_split(id_train_dev, test_size=.125)
-
-#     print(len(id_train))
-#     print(len(id_dev))
-#     print(len(id_test))
-
-    data_df_train = data_df[data_df.sensor_id.isin(id_train)]
-    data_df_dev = data_df[data_df.sensor_id.isin(id_dev)]
-    data_df_test = data_df[data_df.sensor_id.isin(id_test)]
-
-#     print(data_df_train.shape)
-#     print(data_df_dev.shape)
-#     print(data_df_test.shape)
-
-    X_train = data_df_train[columns_to_keep]
-    X_dev = data_df_dev[columns_to_keep]
-    X_test = data_df_test[columns_to_keep]
-    y_train = data_df_train['2_5um']
-    y_dev = data_df_dev['2_5um']
-    y_test = data_df_test['2_5um']
     
-    # fit the data
+    # these colunms are the only ones I'm using in kNN
+    columns_to_keep = ['time_delta', 'lat', 'lon']
+    
+    X_train, X_dev, X_test, y_train, y_dev, y_test = split_data(columns_to_keep)
+    
+    # fit the model
     regr = KNeighborsRegressor(n_neighbors=2**5)
     regr.fit(X_train, y_train)
     
@@ -86,13 +95,12 @@ def test_model(regr, X_dev, y_dev):
     
     
 def save_model(regr, name):
-    from joblib import dump, load
     dump(regr, os.path.join('models', name+'_model.joblib'))
 
 
 if __name__ == '__main__':
     data_df = import_data()
-    regr, X_train, X_dev, X_test, y_train, y_dev, y_test = build_kNN_model()
+    regr, X_train, X_dev, X_test, y_train, y_dev, y_test = build_kNN_model(data_df)
     test_model(regr, X_dev, y_dev)
     save_model(regr, 'kNN')
     
